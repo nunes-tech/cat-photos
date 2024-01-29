44d84324ec4da9b5ef77ee9551d06bc73ef23dab
Nunes <nunestech2@gmail.com>
2024-01-29 14:54:37 -0300

    arquivos locais alterados

--- app/src/test/java/com/example/catphotos/domain/usecase/TheCatApiUseCaseImplTest.kt
+++ app/src/test/java/com/example/catphotos/domain/usecase/TheCatApiUseCaseImplTest.kt
@@ -1,22 +1,56 @@
 package com.example.catphotos.domain.usecase
 
-import org.junit.Assert.*
+import com.example.catphotos.data.dto.TheCatApiDTOItem
+import com.example.catphotos.data.repository.GatosApiRepositoryImpl
+import com.google.common.truth.Truth.assertThat
+import kotlinx.coroutines.test.runTest
 
 import org.junit.After
 import org.junit.Before
 import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mock
+import org.mockito.Mockito
+import org.mockito.MockitoAnnotations
+import org.mockito.junit.MockitoJUnitRunner
 
+@RunWith( MockitoJUnitRunner::class)
 class TheCatApiUseCaseImplTest {
+
+    @Mock
+    private lateinit var gatosApiRepository: GatosApiRepositoryImpl
 
     @Before
     fun setUp() {
+        MockitoAnnotations.openMocks(this)
+    }
+
+    @Test
+    fun invoke_retornaListaDeUrls_retornaLista() = runTest {
+        val theCatApiUseCase = TheCatApiUseCaseImpl(gatosApiRepository)
+
+        //QUANDO
+        Mockito.`when`( gatosApiRepository.getImagesGatosApi() ).thenReturn(
+            listOf(
+                TheCatApiDTOItem(
+                    listOf("Pitbull", "Pastor Alemão"),
+                    400,"f20", "https://gatos.com.br/image1.jpj",500
+                ),
+                TheCatApiDTOItem(
+                    listOf("Rotwailler", "Lavrador"),
+                    600,"q28", "https://gatos.com.br/image2.jpj",400
+                )
+            )
+        )
+
+        //DADO ==============================
+        val lista = theCatApiUseCase()
+
+        //ENTÃO ===========================
+        assertThat(lista).isNotEmpty()
     }
 
     @After
     fun tearDown() {
-    }
-
-    @Test
-    operator fun invoke() {
     }
 }

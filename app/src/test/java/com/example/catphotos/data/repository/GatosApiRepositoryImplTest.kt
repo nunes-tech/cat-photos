267e850f7991dbe23171cb170aa69f9a0ed160ca
Nunes <nunestech2@gmail.com>
2024-01-29 14:43:27 -0300

    Testes Unitarios do Repository, UseCase, e ViewModel + LiveData criados

---
+++ app/src/test/java/com/example/catphotos/data/repository/GatosApiRepositoryImplTest.kt
@@ -0,0 +1,22 @@
+package com.example.catphotos.data.repository
+
+import org.junit.Assert.*
+
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+
+class GatosApiRepositoryImplTest {
+
+    @Before
+    fun setUp() {
+    }
+
+    @After
+    fun tearDown() {
+    }
+
+    @Test
+    fun getImagesGatosApi() {
+    }
+}
---
+++ app/src/test/java/com/example/catphotos/domain/usecase/TheCatApiUseCaseImplTest.kt
@@ -0,0 +1,22 @@
+package com.example.catphotos.domain.usecase
+
+import org.junit.Assert.*
+
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+
+class TheCatApiUseCaseImplTest {
+
+    @Before
+    fun setUp() {
+    }
+
+    @After
+    fun tearDown() {
+    }
+
+    @Test
+    operator fun invoke() {
+    }
+}
